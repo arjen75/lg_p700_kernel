16f9920b36949b4fb5e0910167d207cca308892f
Arjen Visser <arjen.jessica@gmail.com>
2012-12-10 13:33:02 +0100

    First Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 PREEMPT Fri Jun 1 09:30:14 KST 2012"
+#define LINUX_COMPILE_BY "hyungkyu.lim"
+#define LINUX_COMPILE_HOST "anemone"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
